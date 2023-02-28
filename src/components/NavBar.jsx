9f5494f2534d33f071cb941bcb14f635903ff561
PrimeFord <bashfordbash@gmail.com>
2023-02-28 10:47:42 +0100

    Update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav>
-      <Link></Link>
+      <Link to="/">
+        <img src="/image/logo.svg" alt=""></img>
+      </Link>
+      <Link to="/purpose">Purpose</Link>
+      <Link to="/community">Community</Link>
+      <Link to="/career">
+        Careers<sup>[11]</sup>
+      </Link>
+      <Link to="/learn">Learn</Link>
+      <Link to="/united">
+        United Kingdom{" "}
+        <span>
+          <img src="/image/location.svg" alt=""></img>
+        </span>
+      </Link>
     </nav>
   );
 };
